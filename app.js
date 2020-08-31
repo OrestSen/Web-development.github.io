59890584aa543b20c7b932faca83b60ffc180580
orest <oresttrue@gmail.com>
2020-08-31 21:54:29 +0300

    added menu

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ $(document).ready(function(){
     });
     
   });
+  let mobileMenu = document.querySelector(".nav-mobile-menu")
+
+let mainMenu = document.querySelector(".navigation")
+
+mobileMenu.addEventListener("click",function() {
+    mobileMenu.classList.toggle("active-menu");
+    if(mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu")
+    }
+})
