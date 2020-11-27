67db1fbd2dc0561c780a0293b667501dd47c9f43
orest <oresttrue@gmail.com>
2020-11-27 22:35:58 +0200

    modified modal window styleЄ git add .
    Э

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
+let mobileMenu = document.querySelector(".nav-mobile-menu")
+let mainMenu = document.querySelector(".navigation")
+
+mobileMenu.addEventListener("click", function () {
+  mobileMenu.classList.toggle("active-menu");
+  if (mobileMenu.classList.contains("active-menu")) {
+    mainMenu.classList.add("active-menu");
+  } else {
+    mainMenu.classList.remove("active-menu")
+  }
+})
+
+let showMeMore = document.querySelectorAll(".btn__hire");
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".btn-close")
+let default_btn = document.querySelector(".default-btn")
+
+showMeMore.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    modal.classList.add("show");
+    modal.classList.remove("hide")
+  })
+})
+
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+}
+
+closeBtn.addEventListener("click", closeModal)
+default_btn.addEventListener("click", closeModal)
+modal.addEventListener("click", function (e) {
+  if (e.target == modal) {
+    closeModal()
+  }
+})
+
+$('.menu').onePageNav()
+
 $(document).ready(function () {
 
   $(".fa-search").click(function () {
@@ -13,72 +52,5 @@ $('#exampleModal').modal({
   keyboard: true,
 })
 
-let mobileMenu = document.querySelector(".nav-mobile-menu")
-let mainMenu = document.querySelector(".navigation")
-
-mobileMenu.addEventListener("click", function () {
-  mobileMenu.classList.toggle("active-menu");
-  if (mobileMenu.classList.contains("active-menu")) {
-    mainMenu.classList.add("active-menu");
-  } else {
-    mainMenu.classList.remove("active-menu")
-  }
-})
 
 // *******************************************************************
-
-// let showMeMore = document.querySelector(".ourservices-btn");
-// let modal = document.querySelector(".modal_1");
-// let btn__close = document.querySelector(".btn-close");
-// let send = document.querySelector(".default-btn");
-
-// showMeMore.forEach(function (btn) {
-//   btn.addEventListener("click", openModal)
-// })
-
-// function openModal() {
-//   modal.classList.remove("hide")
-//   modal.classList.add("show")
-
-
-// }
-// function closeModal() {
-//   modal.classList.remove("show")
-//   modal.classList.add("hide")
-// }
-
-// btn__close.addEventListener("click", closeModal)
-// send.addEventListener("click", closeModal)
-
-// modal.addEventListener("click", function (e) {
-//   if (e.target == modal) {
-//     closeModal()
-//   }
-// })
-
-// ********************************
-
-let showMeMore = document.querySelectorAll(".ourservices-btn");
-let modal = document.querySelector(".modal_1")
-let closeBtn = document.querySelector(".btn-close")
-let pleaseCall = document.querySelector(".pleaseCall")
-
-showMeMore.forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    modal.classList.add("show");
-    modal.classList.remove("hide")
-  })
-})
-
-function closeModal() {
-  modal.classList.add("hide");
-  modal.classList.remove("show");
-}
-
-closeBtn.addEventListener("click", closeModal)
-pleaseCall.addEventListener("click", closeModal)
-modal.addEventListener("click", function (e) {
-  if (e.target == modal) {
-    closeModal()
-  }
-})
